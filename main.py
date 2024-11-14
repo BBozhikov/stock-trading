38386ec804911ba8e1390ce1fa6e04c753eb9ec8
BBozhikov <125923082+BBozhikov@users.noreply.github.com>
2024-11-15 01:48:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -76,25 +76,4 @@ else:
         print(f"Tesla prices has fallen by {round(perc)}%🔻")
         get_news()
 
-# strftime can be used on a datetime to make it 0 padded like 06.06.2024
-
-# STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number.
-# No I don't want
-
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file 
-by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
-coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file
-by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
-coronavirus market crash.
-"""
 
